a3bc5bf7d030f19115e341b10143c96f3d6ee4c2
dev-yongseok <yys0863@naver.com>
2020-04-09 00:34:07 +0900

    Load More 시 메인 이미지가 변경되는 것을 고침

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,209 +1,84 @@
-import React, { useEffect, useState, useRef } from 'react'
-
-import { Typography, Row, Button } from 'antd';
-
-import { API_URL, API_KEY, IMAGE_BASE_URL, IMAGE_SIZE, POSTER_SIZE } from '../../Config'
-
-import MainImage from './Sections/MainImage'
-
-import GridCard from '../commons/GridCards'
-
-const { Title } = Typography;
+import React, {useEffect, useState} from 'react'
+import { FaCode } from "react-icons/fa";
+import { API_URL , API_KEY, IMAGE_BASE_URL} from "../../Config";
+import MainImage from "./Sections/MainImage";
+import axios from 'axios';
+import GridCards from '../commons/GridCards';
+import {Row} from 'antd';
 
 function LandingPage() {
 
-    const buttonRef = useRef(null);
-
-    const [Movies, setMovies] = useState([])
-
-    const [MainMovieImage, setMainMovieImage] = useState(null)
-
-    const [Loading, setLoading] = useState(true)
-
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(0);
     const [CurrentPage, setCurrentPage] = useState(0)
 
-
-
     useEffect(() => {
-
+        // movie/popular?api_key=<<api_key>>&language=en-US&page=1
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
-        fetchMovies(endpoint)
-
-    }, [])
-
-
-
-    useEffect(() => {
-
-        window.addEventListener("scroll", handleScroll);
-
-    }, [])
-
-
-
-
-    const fetchMovies = (endpoint) => {
-
-
-
-        fetch(endpoint)
-
-            .then(result => result.json())
-
-            .then(result => {
-
-                // console.log(result)
-
-                // console.log('Movies',...Movies)
-
-                // console.log('result',...result.results)
-
-                setMovies([...Movies, ...result.results])
-
-                setMainMovieImage(MainMovieImage || result.results[0])
-
-                setCurrentPage(result.page)
-
-            }, setLoading(false))
-
-            .catch(error => console.error('Error:', error)
-
-            )
-
-    }
-
-
-
-    const loadMoreItems = () => {
-
-        let endpoint = '';
-
-        setLoading(true)
-
-        console.log('CurrentPage', CurrentPage)
-
-        endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
-
         fetchMovies(endpoint);
 
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                setMainMovieImage(response.results[0]);
+            })
 
+    }, []);
 
+    const fetchMovies = (endpoint) =>{
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response.results);
+                setMovies([...Movies , ...response.results]);
+                setCurrentPage(response.page)
+            })
+    }
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage+1}`;
+
+        fetchMovies(endpoint);
     }
 
 
-
-    const handleScroll = () => {
-
-        const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-
-        const body = document.body;
-
-        const html = document.documentElement;
-
-        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-        const windowBottom = windowHeight + window.pageYOffset;
-
-        if (windowBottom >= docHeight - 1) {
-
-
-
-            // loadMoreItems()
-
-            console.log('clicked')
-
-            buttonRef.current.click();
-
-
-
-        }
-
-    }
-
-
-
-    return (
-
-        <div style={{ width: '100%', margin: '0' }}>
-
+    return  (
+        <div style={{width:'100%' , margin : '0'}}>
+            {/* Main Image */}
             {MainMovieImage &&
-
                 <MainImage
-
-                    image={`${IMAGE_BASE_URL}${IMAGE_SIZE}${MainMovieImage.backdrop_path}`}
-
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
                     title={MainMovieImage.original_title}
-
                     text={MainMovieImage.overview}
-
                 />
-
-
-
             }
-
-
-
-            <div style={{ width: '85%', margin: '1rem auto' }}>
-
-
-
-                <Title level={2} > Movies by latest </Title>
-
+            <div style={{width:'85%' , margin : '1rem auto'}}>
+                <h2>최근 개봉 영화</h2>
                 <hr />
+                {/* Movie Grid Cards*/}
 
-                <Row gutter={[16, 16]}>
+                <Row gutter={[16,16]}>
 
-                    {Movies && Movies.map((movie, index) => (
-
+                    {Movies && Movies.map((movie, index) =>(
                         <React.Fragment key={index}>
-
-                            <GridCard
-
+                            <GridCards 
                                 image={movie.poster_path ?
-
-                                    `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-
-                                    : null}
-
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                                 movieId={movie.id}
-
                                 movieName={movie.original_title}
-
                             />
-
                         </React.Fragment>
-
-                    ))}
-
-                </Row>
-
-
-
-                {Loading &&
-
-                    <div>Loading...</div>}
-
-
-
-                <br />
-
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-
-                    <button ref={buttonRef} className="loadMore" onClick={loadMoreItems}>Load More</button>
-
-                </div>
-
+                        
+                    ))}    
+                
+                </Row>                
             </div>
 
-
+            <div style={{display : 'flex', justifyContent : 'center'}}>
+                <button onClick={loadMoreItems}> Load More </button>
+            </div>
         </div>
-
     )
-
 }
-
-
 
 export default LandingPage
